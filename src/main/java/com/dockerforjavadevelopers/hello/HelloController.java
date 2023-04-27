6a90808842903359d153729cadba101c6c8cfbcf
Kathiresan Chinnasamy <k.chinnasamy@sharecare.com>
2023-04-27 21:00:13 +0530

    initial commit

---
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -0,0 +1,20 @@
+package com.dockerforjavadevelopers.hello;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World\n";
+    }
+    
+    @RequestMapping("/health-check")
+    public String index1() {
+        return "Hello World\n";
+    }
+    
+}
